d7f0b6f00e7542eb13c2c602ea2bfb3accc4a59c
Rayane Troudi <rayanetroudi1@gmail.com>
2023-01-05 10:31:17 +0100

    DCT OK, Compression POK, Decompression NOK

--- main.py
+++ main.py
@@ -30,9 +30,14 @@ def getImage(path_image):
         im_bad_ext = Image.open(path_image)
         im_bad_ext.convert('RGB')
         good_img = im_bad_ext.save(res[2]+'.jpg')
-    return plt.imread(res[2]+'.jpg')
+    return plt.imread(res[2]+'.jpg') # renvoie un tableau de tableau de pixels
 
 
+
+
+#############################################
+#  DEBUT PROJET FONCTION                    #
+#############################################
 
 def tronquerImage(path_image):
     
@@ -58,19 +63,18 @@ def tronquerImage(path_image):
 
             new_matrix_2D[i][j] = img_matrix[i][j][0]
          
-    print(np.shape(new_matrix_2D))   
-    return new_matrix_2D
+    return np.array(new_matrix_2D).astype(float)
     
 
 def centrageSurOctet(matrix_img):
     dim = np.shape(matrix_img)
-    new_matrix_2D = [[0]*dim[1]]*dim[0]
+    new_matrix_2D = np.array([[0]*dim[1]]*dim[0])
     for i in range(0,dim[0]):
         for j in range(0,dim[1]):
           
-            new_matrix_2D[i][j] = matrix_img[i][j] - 128
+            new_matrix_2D[i,j] = matrix_img[i,j] - 128
     
-    return new_matrix_2D
+    return np.array(new_matrix_2D).astype(float)
 
 
 def matrixPassageOrtho(n):
@@ -82,7 +86,7 @@ def matrixPassageOrtho(n):
                 P[i][j] = (1/2) * C0 * np.cos((2*j + 1) * i * mp.pi/16)
             else:
                 P[i][j] = (1/2) * mp.cos((2*j + 1) * i * mp.pi/16)
-    return np.array(P)
+    return np.array(P).astype(float)
 
 
 def extraireBloc(i,j, matrix_image):
@@ -96,18 +100,19 @@ def extraireBloc(i,j, matrix_image):
             v = v + 1
         u = u + 1
         v = 0
-    return np.array(bloc)
+    return np.array(bloc).astype(float)
 
 
 def chgtBaseBloc(bloc,P, Pt):
-    return np.dot(np.dot(P,bloc),Pt)
+    return (np.dot(np.dot(P,bloc),Pt)).astype(float)
+
 
 def ecrireBlocToMatrix(matrix_image_2D,bloc,i,j):
     u = 0
     v = 0
     for k in range(i,i+8):
         for p in range(j,j+8):
-            matrix_image_2D[k][p] = bloc[u][v]
+            matrix_image_2D[k,p] = bloc[u,v]
             v = v + 1
         u = u + 1
         v = 0
@@ -119,7 +124,7 @@ def DiscretCosineTransform(matrix_image_2D,P,Pt):
     width = dim[0]
     height = dim[1]
     
-    new_matrix_dct = [[0]*height]*width
+    new_matrix_dct = np.array([[0]*height]*width)
 
     for i in range(0,16,8):
         for j in range(0,16,8):
@@ -127,34 +132,45 @@ def DiscretCosineTransform(matrix_image_2D,P,Pt):
             bloc_dct = chgtBaseBloc(bloc,P,Pt)
             ecrireBlocToMatrix(new_matrix_dct,bloc_dct,i,j)
         
-    return np.array(new_matrix_dct)
+    return np.array(new_matrix_dct).astype(float)
       
       
+Q = np.array([ [16,11,10,16,24,40,51,61],
+     [12,12,13,19,26,58,60,55],
+     [14,13,16,24,40,57,69,56],
+     [14,17,22,29,51,87,80,62],
+     [18,22,37,56,68,109,103,77],
+     [24,35,55,64,81,104,113,92],
+     [49,64,78,87,103,121,120,101],
+     [72,92,95,98,112,100,103,99]])
+
+
+def DivideByTerm(Q,D):
+    return np.array(np.divide(D,Q))
+
+
+
+def Compression(matrix_img_2D):
+    
+    print("test")
+
+
+
 
 img1 = tronquerImage(IMG_PATH_1) 
 img2 = centrageSurOctet(img1)
 
-test = np.zeros((600,1200))
-for i in range(0,600):
-    for j in range(0,1200):
-        test[i][j] = img2[i][j]
-
 P = matrixPassageOrtho(8)
 Pt = P.transpose()
+K = img2[0:8,0:8]
 
-test1 = DiscretCosineTransform(test,P,Pt)
-print(test1.astype(int))
+test1 = DiscretCosineTransform(img2,P,Pt)
+res = DivideByTerm(Q,test1)
+print(res)
 
-#print(t.astype(int))
-plt.imsave('./test.jpg',np.array(test1))
-# test = np.zeros((16,16))
-# bloc = 2*np.ones((8,8))
-# ecrireBlocToMatrix(test,bloc,0,0)
-# ecrireBlocToMatrix(test,bloc,0,8)
-# ecrireBlocToMatrix(test,bloc,8,8)
-# ecrireBlocToMatrix(test,bloc,8,0)
-# test1 = DiscretCosineTransform(test,bloc,bloc)
-# print(np.array(test1))
+
+
+
 
 
         
